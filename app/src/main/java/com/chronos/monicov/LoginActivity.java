b155a7f9acb68cfc2669f9a3fa0fa4feb38ff414
Romeo Encinares <romeoncrs@gmail.com>
2022-04-27 21:11:51 +0800

    Create initial activity to facilitate user login

---
+++ app/src/main/java/com/chronos/monicov/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.chronos.monicov;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
