c4e43efe289fa49622d97f01262d6aee2a145941
Romeo Encinares <romeoncrs@gmail.com>
2022-04-27 21:08:49 +0800

    Add get started button

--- app/src/main/java/com/chronos/monicov/MainActivity.java
+++ app/src/main/java/com/chronos/monicov/MainActivity.java
@@ -2,14 +2,22 @@ package com.chronos.monicov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseAuth mAuth;
+
+    Button get_Started;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,25 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
 
+        get_Started = findViewById(R.id.get_Started);
+
+        get_Started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            currentUser.reload();
+//        }
+//    }
 }
