6f99061ecb835da4ae07e7ec90c87f8477ab80e3
Romeo Encinares <romeoncrs@gmail.com>
2022-04-28 15:15:52 +0800

    Create function to create user object in firebase realtime database

--- app/src/main/java/com/chronos/monicov/RegisterActivity.java
+++ app/src/main/java/com/chronos/monicov/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,6 +26,9 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText etRegPassword;
     TextView tvLoginHere;
     Button btnRegister;
+    private DatabaseReference mDatabase;
+    private FirebaseDatabase database;
+    private static final String USER = "user";
 
     FirebaseAuth mAuth;
 
@@ -36,10 +42,13 @@ public class RegisterActivity extends AppCompatActivity {
         tvLoginHere = findViewById(R.id.tvLoginHere);
         btnRegister = findViewById(R.id.btnRegister);
 
+        database = FirebaseDatabase.getInstance();
+        mDatabase = database.getReference(USER);
         mAuth = FirebaseAuth.getInstance();
 
         btnRegister.setOnClickListener(view ->{
             createUser();
+            createUserObject();
         });
 
         tvLoginHere.setOnClickListener(view ->{
@@ -47,6 +56,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    // Create User in Firebase Authentication
     private void createUser() {
         String email = etRegEmail.getText().toString();
         String password = etRegPassword.getText().toString();
@@ -71,6 +81,25 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+
+    //Create user Object in Firebase Realtime Database
+    private void createUserObject() {
+        String email = etRegEmail.getText().toString();
+        String password = etRegPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Password cannot be empty");
+            etRegPassword.requestFocus();
+        }else{
+            User user = new User(email, password);
+            String keyId = mDatabase.push().getKey();
+            mDatabase.child(keyId).setValue(user);
+        }
+
     }
 
 }
