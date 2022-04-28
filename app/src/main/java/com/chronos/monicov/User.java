1414b4178f86698bf5eb9e2ecbdaffd4efcdc95a
Romeo Encinares <romeoncrs@gmail.com>
2022-04-28 18:56:09 +0800

    Add firstname, lastname, usertype in user attributes

--- app/src/main/java/com/chronos/monicov/User.java
+++ app/src/main/java/com/chronos/monicov/User.java
@@ -3,13 +3,19 @@ package com.chronos.monicov;
 public class User {
     private String email;
     private String password;
+    private String firstname;
+    private String lastname;
+    private String userType;
 
     public User() {
     }
 
-    public User(String email, String password) {
+    public User(String email, String password, String userType, String firstname, String lastname) {
         this.email = email;
         this.password = password;
+        this.userType = userType;
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
     public String getEmail() {
@@ -18,5 +24,17 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+    
+    public String getUserType() {
+        return userType;
     }
 }
