d97da8e8f0cb02e45472e8ed2ec6489636fb8cf5
Romeo Encinares <romeoncrs@gmail.com>
2022-04-29 13:06:42 +0800

    Implement user login through firebase

--- app/src/main/java/com/chronos/monicov/LoginActivity.java
+++ app/src/main/java/com/chronos/monicov/LoginActivity.java
@@ -1,12 +1,77 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText emailField;
+    TextInputEditText passwordField;
+    TextView registerText;
+    Button loginButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailField = findViewById(R.id.etLoginEmail);
+        passwordField = findViewById(R.id.etLoginPass);
+        loginButton = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(view -> {
+            loginUser();
+        });
+    }
+
+    public void loginUser(){
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        if (checkFields(email, password)){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this,"User sign in successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Sign in Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    public boolean checkFields(String email, String password){
+        if (TextUtils.isEmpty(email)){
+            emailField.setError("Email cannot be empty");
+            emailField.requestFocus();
+        }
+        else if (TextUtils.isEmpty(password)) {
+            passwordField.setError("Email cannot be empty");
+            passwordField.requestFocus();
+        }
+        else {
+            return true;
+        }
+        return false;
     }
 }
