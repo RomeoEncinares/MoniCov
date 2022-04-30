cf4bca4befcc60af1ee76d3e13434dd531d30474
Romeo Encinares <romeoncrs@gmail.com>
2022-04-30 18:52:59 +0800

    Add logout function

--- app/src/main/java/com/chronos/monicov/medicalProfessionalHomeActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalHomeActivity.java
@@ -2,13 +2,33 @@ package com.chronos.monicov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class medicalProfessionalHomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_professional);
+
+        mAuth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logoutMedicalProfessional);
+
+        logoutButton.setOnClickListener(view -> {
+            logout();
+        });
+    }
+
+    public void logout(){
+        mAuth.signOut();
+        startActivity(new Intent(medicalProfessionalHomeActivity.this, LandingActivity.class));
     }
 }
