5d20a12429dfc0c09e142e6235a572ec62b69da2
Romeo Encinares <romeoncrs@gmail.com>
2022-04-30 18:07:47 +0800

    Create initial activity for medical professional

---
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalHomeActivity.java
@@ -0,0 +1,14 @@
+package com.chronos.monicov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medicalProfessionalHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_professional);
+    }
+}
