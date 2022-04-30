febda61740622383be7bad2c7c0938e469feb1e1
Romeo Encinares <romeoncrs@gmail.com>
2022-04-30 14:15:10 +0800

    Convert attributes from private to protected to enable access from subclass

--- app/src/main/java/com/chronos/monicov/User.java
+++ app/src/main/java/com/chronos/monicov/User.java
@@ -18,4 +18,23 @@ public class User {
         this.lastname = lastname;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
 }
