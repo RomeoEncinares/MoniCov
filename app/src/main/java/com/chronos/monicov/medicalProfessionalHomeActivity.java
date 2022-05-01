551faf037a02c88b888154c2c4447ad41e3a40f7
Romeo Encinares <romeoncrs@gmail.com>
2022-05-01 15:56:09 +0800

    Create function generateCode

--- app/src/main/java/com/chronos/monicov/medicalProfessionalHomeActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalHomeActivity.java
@@ -1,18 +1,31 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
 
 public class medicalProfessionalHomeActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private DatabaseReference medicalProfessionalNode, patientListNode;
+    private FirebaseDatabase database;
 
     Button logoutButton;
+    Button generateCodeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +34,21 @@ public class medicalProfessionalHomeActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         logoutButton = findViewById(R.id.logoutMedicalProfessional);
+        generateCodeButton = findViewById(R.id.generateCode);
+        database = FirebaseDatabase.getInstance();
 
-        logoutButton.setOnClickListener(view -> {
-            logout();
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+        generateCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = generateCode();
+            }
         });
     }
 
@@ -31,4 +56,14 @@ public class medicalProfessionalHomeActivity extends AppCompatActivity {
         mAuth.signOut();
         startActivity(new Intent(medicalProfessionalHomeActivity.this, LandingActivity.class));
     }
+
+    public String generateCode(){
+        int max = 60000;
+        int min = 10000;
+        int code;
+        code = (int) (Math.random() * (max - min + 1) + min);
+        return String.valueOf(code);
+    }
+
+
 }
