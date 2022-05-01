753dcd65b3db772a4bb42cb51ec2a06ed5b3c9d0
Romeo Encinares <romeoncrs@gmail.com>
2022-05-01 19:24:52 +0800

    Create addPatient function to add user to list of patients

--- app/src/main/java/com/chronos/monicov/RegisterActivity.java
+++ app/src/main/java/com/chronos/monicov/RegisterActivity.java
@@ -19,8 +19,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -28,9 +31,10 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
     TextInputEditText passwordField;
     TextInputEditText firstNameField;
     TextInputEditText lastNameField;
+    TextInputEditText medicalProfessionalEmailField;
     TextView loginText;
     Button registerButton;
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase, medicalProfessionalNode, patientListNode;;
     private FirebaseDatabase database;
     private Spinner spinner;
 
@@ -45,6 +49,7 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         passwordField = findViewById(R.id.etRegPass);
         firstNameField = findViewById(R.id.etFirstName);
         lastNameField = findViewById(R.id.etLastName);
+        medicalProfessionalEmailField = findViewById(R.id.etmedicalProfessionalEmail);
         loginText = findViewById(R.id.tvLoginHere);
         registerButton = findViewById(R.id.btnRegister);
         spinner = findViewById(R.id.user_type);
@@ -58,30 +63,30 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
         spinner.setOnItemSelectedListener(this);
 
-        registerButton.setOnClickListener(view ->{
+        registerButton.setOnClickListener(view -> {
             createUser();
         });
 
-        loginText.setOnClickListener(view ->{
+        loginText.setOnClickListener(view -> {
             startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
         });
     }
 
     // Create User in Firebase Authentication
     private void addFirebaseUser(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email,password)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(RegisterActivity.this,"User registered successful", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                            }else{
-                                Toast.makeText(RegisterActivity.this, "Registration Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "User registered successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Registration Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    });
-        }
+                    }
+                });
+    }
 
     //Create User
     private void createUser() {
@@ -90,22 +95,20 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         String userType = spinner.getSelectedItem().toString();
         String firstName = firstNameField.getText().toString();
         String lastName = lastNameField.getText().toString();
+        String medicalProfessionalEmail = medicalProfessionalEmailField.getText().toString();
 
-
-        if (checkFields(email, password, firstName, lastName)){
+        if (checkFields(email, password, firstName, lastName, medicalProfessionalEmail)) {
             addFirebaseUser(email, password);
-            if (userType.equals("Patient")){
+            if (userType.equals("Patient")) {
                 Patient patient = new Patient(email, password, userType, firstName, lastName);
                 mDatabase = database.getReference("Patient");
-//                String keyId = mDatabase.push().getKey();
                 String keyId = email.replace(".", "");
                 mDatabase.child(keyId).setValue(patient);
-                System.out.println(userType);
-            }
-            else{
-                MedicalProfessional medicalProfessional = new MedicalProfessional(email, password, userType, firstName, lastName);
                 mDatabase = database.getReference("Medical Professional");
-//                String keyId = mDatabase.push().getKey();
+                // Add to Medical Professional Patient List
+                addPatient(medicalProfessionalEmail, email, firstName, lastName);
+            } else {
+                MedicalProfessional medicalProfessional = new MedicalProfessional(email, password, userType, firstName, lastName);
                 String keyId = email.replace(".", "");
                 mDatabase.child(keyId).setValue(medicalProfessional);
             }
@@ -125,26 +128,37 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
     }
 
-    public boolean checkFields(String email, String password, String firstName, String lastName){
-        if (TextUtils.isEmpty(email)){
+    public boolean checkFields(String email, String password, String firstName, String lastName, String medicalProfessionalEmail) {
+        if (TextUtils.isEmpty(email)) {
             emailField.setError("Email cannot be empty");
             emailField.requestFocus();
-        }
-        else if (TextUtils.isEmpty(password)){
+        } else if (TextUtils.isEmpty(password)) {
             passwordField.setError("Email cannot be empty");
             passwordField.requestFocus();
-        }
-        else if (TextUtils.isEmpty(firstName)){
+        } else if (TextUtils.isEmpty(firstName)) {
             firstNameField.setError("Email cannot be empty");
             firstNameField.requestFocus();
-        }
-        else if (TextUtils.isEmpty(lastName)){
+        } else if (TextUtils.isEmpty(lastName)) {
             lastNameField.setError("Email cannot be empty");
             lastNameField.requestFocus();
+        }
+        else if (TextUtils.isEmpty(medicalProfessionalEmail)) {
+            medicalProfessionalEmailField.setError("Email cannot be empty");
+            medicalProfessionalEmailField.requestFocus();
         }
         else {
             return true;
         }
         return false;
+    }
+
+    public void addPatient(String reference, String patientEmail, String patientFirstname, String patientLastname){
+        MedicalProfessional.assignedPatient newPatient = new MedicalProfessional.assignedPatient(patientEmail, patientFirstname, patientLastname);
+        mDatabase = database.getReference("Medical Professional");
+        String targetReference = reference.replace(".", "");
+        medicalProfessionalNode = mDatabase.child(targetReference);
+        patientListNode = medicalProfessionalNode.child("Patient List");
+        String keyId = patientEmail.replace(".", "");
+        patientListNode.child(keyId).setValue(newPatient);
     }
 }
