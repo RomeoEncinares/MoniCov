893dd205424d7167fbdc679e56508aec2e97d913
Romeo Encinares <romeoncrs@gmail.com>
2022-05-03 15:41:28 +0800

    Display firstname and lastname of logged in patient

--- app/src/main/java/com/chronos/monicov/patientHomeActivity.java
+++ app/src/main/java/com/chronos/monicov/patientHomeActivity.java
@@ -1,21 +1,31 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class patientHomeActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     Button logoutButton;
     ImageButton homeButton;
+    TextView firstNameTextField, lastNameTextField;
 
 
     @Override
@@ -26,6 +36,10 @@ public class patientHomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         logoutButton = findViewById(R.id.logoutPatient);
         homeButton = findViewById(R.id.patientHomeButton);
+        firstNameTextField = findViewById(R.id.firstNameText);
+        lastNameTextField = findViewById(R.id.lastNameText);
+
+        queryData(getCurrentPatient());
 
         logoutButton.setOnClickListener(view -> {
             logout();
@@ -40,5 +54,31 @@ public class patientHomeActivity extends AppCompatActivity {
     public void logout(){
         mAuth.signOut();
         startActivity(new Intent(patientHomeActivity.this, LandingActivity.class));
+    }
+
+    public String getCurrentPatient(){
+        String currentUser = mAuth.getCurrentUser().getEmail().toString();return currentUser;
+    }
+
+    public void queryData(String email){
+        String emailKey = email.replace(".", "");
+        mDatabase = FirebaseDatabase.getInstance().getReference("Patient").child(emailKey);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String, String> map = (HashMap<String, String>) snapshot.getValue();
+                String firstName = map.get("firstname");
+                String lastName = map.get("lastname");
+                char lastNameChar = lastName.charAt(0);
+
+                firstNameTextField.setText(firstName);
+                lastNameTextField.setText(String.valueOf(lastNameChar));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
