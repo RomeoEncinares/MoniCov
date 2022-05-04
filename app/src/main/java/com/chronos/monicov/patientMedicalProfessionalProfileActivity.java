e966778c7e95778f8e2799fc50eebcf4d1e45d45
Romeo Encinares <romeoncrs@gmail.com>
2022-05-04 21:38:28 +0800

    Create initial activity

---
+++ app/src/main/java/com/chronos/monicov/patientMedicalProfessionalProfileActivity.java
@@ -0,0 +1,14 @@
+package com.chronos.monicov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patientMedicalProfessionalProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_medical_professional_profile);
+    }
+}
