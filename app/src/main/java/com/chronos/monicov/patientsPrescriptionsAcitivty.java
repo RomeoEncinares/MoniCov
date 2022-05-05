aa7753e08b4c2b2035a9c154044c79ca7e7cb43a
Romeo Encinares <romeoncrs@gmail.com>
2022-05-05 20:57:46 +0800

    Create initial activity

---
+++ app/src/main/java/com/chronos/monicov/patientsPrescriptionsAcitivty.java
@@ -0,0 +1,14 @@
+package com.chronos.monicov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patientsPrescriptionsAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patients_prescriptions_acitivty);
+    }
+}
