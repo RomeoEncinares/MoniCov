9794db55ff4f0481f9465ddbdc0ba60eee2e1f79
Romeo Encinares <romeoncrs@gmail.com>
2022-05-05 19:24:23 +0800

    Add medicalProfessionalEmail on patient attributes

--- app/src/main/java/com/chronos/monicov/RegisterIfPatientActivity.java
+++ app/src/main/java/com/chronos/monicov/RegisterIfPatientActivity.java
@@ -8,14 +8,18 @@ import android.text.TextUtils;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterIfPatientActivity extends AppCompatActivity {
 
     TextInputEditText medicalProfessionalEmailField;
-    private DatabaseReference mDatabase, medicalProfessionalNode, patientListNode;;
+    private DatabaseReference mDatabase, medicalProfessionalNode, patientListNode, parentPatientNode;;
     private FirebaseDatabase database;
+    FirebaseAuth mAuth;
 
     Button registerButton;
 
@@ -26,6 +30,7 @@ public class RegisterIfPatientActivity extends AppCompatActivity {
 
         medicalProfessionalEmailField = findViewById(R.id.etmedicalProfessionalEmail);
         database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         registerButton = findViewById(R.id.btnRegister);
 
@@ -48,6 +53,7 @@ public class RegisterIfPatientActivity extends AppCompatActivity {
 
         if (checkFields(medicalProfessionalEmail)){
             addPatient(medicalProfessionalEmail, email, firstName, lastName);
+            addMedical(email, medicalProfessionalEmail);
             startActivity(new Intent(RegisterIfPatientActivity.this, LoginActivity.class));
         }
     }
@@ -71,5 +77,17 @@ public class RegisterIfPatientActivity extends AppCompatActivity {
         patientListNode = medicalProfessionalNode.child("Patient List");
         String keyId = patientEmail.replace(".", "");
         patientListNode.child(keyId).setValue(newPatient);
+
+
+    }
+
+    public void addMedical(String reference, String medicalProfessionalEmail){
+        HashMap map = new HashMap();
+        map.put("medicalProfessionalEmail", medicalProfessionalEmail);
+
+        mDatabase = database.getReference("Patient");
+        String targetReference = reference.replace(".", "");
+        parentPatientNode = mDatabase.child(targetReference);
+        parentPatientNode.updateChildren(map);
     }
 }
