6517ca4ae4edc082ade4f20a567b79a0516f266c
Romeo Encinares <romeoncrs@gmail.com>
2022-05-11 14:41:24 +0800

    Add taskbar button routes

--- app/src/main/java/com/chronos/monicov/patientStatusActivity.java
+++ app/src/main/java/com/chronos/monicov/patientStatusActivity.java
@@ -2,13 +2,43 @@ package com.chronos.monicov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class patientStatusActivity extends AppCompatActivity {
+
+    ImageButton homeButton, profileButton, statusButton, medicalProfessionalButton, settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_status);
+
+        homeButton = findViewById(R.id.patientHomeButton);
+        profileButton = findViewById(R.id.patientProfileButton);
+        statusButton = findViewById(R.id.patientAddStatusButton);
+        medicalProfessionalButton = findViewById(R.id.patientMedicalButton);
+        settingsButton = findViewById(R.id.patientSettingsButton);
+
+        homeButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientStatusActivity.this, patientHomeActivity.class));
+        });
+
+        profileButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientStatusActivity.this, patientProfileActivity.class));
+        });
+
+        statusButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientStatusActivity.this, patientStatusActivity.class));
+        });
+
+        medicalProfessionalButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientStatusActivity.this, patientMedicalProfessionalProfileActivity.class));
+        });
+
+        settingsButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientStatusActivity.this, patientSettingsActivity.class));
+        });
     }
 }
