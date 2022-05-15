e687a5f3a5ca4547e20f02471968b8e12e817921
Romeo Encinares <romeoncrs@gmail.com>
2022-05-15 18:57:52 +0800

    Create function to save genereated qr code as image in pictures

--- app/src/main/java/com/chronos/monicov/patientStatusGenerateQRActivity.java
+++ app/src/main/java/com/chronos/monicov/patientStatusGenerateQRActivity.java
@@ -1,23 +1,45 @@
 package com.chronos.monicov;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.encoder.QRCode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class patientStatusGenerateQRActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     ImageView qrCodeImageField;
+    Button saveImageButton;
+    OutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +48,14 @@ public class patientStatusGenerateQRActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         qrCodeImageField = findViewById(R.id.qrCode);
+        saveImageButton = findViewById(R.id.btnSaveImage);
 
         String currentUser = getCurrentPatient();
         generateQRCode(currentUser);
+
+        saveImageButton.setOnClickListener(view -> {
+            saveImage();
+        });
     }
 
     public void generateQRCode(String currentUser){
@@ -45,6 +72,30 @@ public class patientStatusGenerateQRActivity extends AppCompatActivity {
             }
             qrCodeImageField.setImageBitmap(bmp);
         } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveImage() {
+        BitmapDrawable drawable = (BitmapDrawable) qrCodeImageField.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        System.out.println(filepath);
+//        File dir = new File(filepath.getAbsolutePath()+"/QR_Code");
+//        dir.mkdir();
+        File file = new File(filepath, System.currentTimeMillis()+".jpg");
+        try {
+            outputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(bitmap);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        Toast.makeText(getApplicationContext(), "QR code saved", Toast.LENGTH_SHORT).show();
+        try {
+            outputStream.flush();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
