74af89ebad173609a488a5b87b278b5ead0af6a9
Romeo Encinares <romeoncrs@gmail.com>
2022-05-15 21:31:36 +0800

    Create function to scan qr code using code-scanner library

---
+++ app/src/main/java/com/chronos/monicov/acquaintanceScanActivity.java
@@ -0,0 +1,63 @@
+package com.chronos.monicov;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class acquaintanceScanActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    TextView userEmailField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acquaintance_scan);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        userEmailField = findViewById(R.id.textView25);
+
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        userEmailField.setText(result.getText());
+                        Toast.makeText(acquaintanceScanActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+
+}
