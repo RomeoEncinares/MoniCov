e0abf4e33649614c7fd4773076c632f0be1ce8c6
Romeo Encinares <romeoncrs@gmail.com>
2022-05-16 17:15:03 +0800

    Add function to push data of medical professional profile data to firebase

--- app/src/main/java/com/chronos/monicov/medicalProfessionalProfileActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalProfileActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,12 +22,16 @@ import java.util.HashMap;
 public class medicalProfessionalProfileActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    private DatabaseReference mDatabase, parentPatientNode;
+    private DatabaseReference mDatabase, parentMedicalNode;
     private FirebaseDatabase database;
 
     Button updateButton;
     ImageButton homeButton, profileButton, medicalProfessionalButton, settingsButton, patientsButton;
     TextView firstNameTextField, lastNameTextField, nameInitialTextField;
+    TextInputEditText bioField, ageField, contactNumberField, genderField, birthDateField, addressField,
+        patientsNumberField, experienceField, hospitalNameField, hospitalAddressField,
+        hospitalContactNumberField, vaccineCardNumberField, vaccineBrandField,
+        vaccineDate1Field, vaccineDate2Field, healthFacilityField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +51,31 @@ public class medicalProfessionalProfileActivity extends AppCompatActivity {
         medicalProfessionalButton = findViewById(R.id.medicalNotificationButton);
         settingsButton = findViewById(R.id.medicalSettingsButton);
 
+        bioField = findViewById(R.id.describeYourself);
+        ageField = findViewById(R.id.etAge);
+        genderField = findViewById(R.id.etGender);
+        contactNumberField = findViewById(R.id.etcontactNumber);
+        birthDateField = findViewById(R.id.etBirthDate);
+        addressField = findViewById(R.id.etAddress);
+        patientsNumberField = findViewById(R.id.etNumberOfPatients);
+        experienceField = findViewById(R.id.etExperience);
+        hospitalNameField = findViewById(R.id.etHospitalName);
+        hospitalAddressField = findViewById(R.id.etHospitalAddress);
+        hospitalContactNumberField = findViewById(R.id.etHospitalContact);
+        vaccineCardNumberField = findViewById(R.id.etVaccCardNumber);
+        vaccineBrandField = findViewById(R.id.etVaccName);
+        vaccineDate1Field = findViewById(R.id.etFirstDose);
+        vaccineDate2Field = findViewById(R.id.etSecondDose);
+        healthFacilityField = findViewById(R.id.etHealthFacilityName);
+
+        updateButton = findViewById(R.id.btnUpdate);
+
         String currentUser = getCurrentMedical();
         queryData(currentUser);
+
+        updateButton.setOnClickListener(view -> {
+            addMedicalProfessionalProfile(currentUser);
+        });
 
         homeButton.setOnClickListener(view -> {
             startActivity(new Intent(medicalProfessionalProfileActivity.this, medicalProfessionalHomeActivity.class));
@@ -97,5 +125,47 @@ public class medicalProfessionalProfileActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void addMedicalProfessionalProfile(String reference){
+        String bio = bioField.getText().toString();
+        String age = ageField.getText().toString();
+        String gender = genderField.getText().toString();
+        String contact = contactNumberField.getText().toString();
+        String birthDate = birthDateField.getText().toString();
+        String address = addressField.getText().toString();
+        String patientNumber = patientsNumberField.getText().toString();
+        String experience = experienceField.getText().toString();
+        String hospitalName = hospitalNameField.getText().toString();
+        String hospitalAddress = hospitalAddressField.getText().toString();
+        String hospitalContactNumber = hospitalContactNumberField.getText().toString();
+        String vaccineCardNumber = vaccineCardNumberField.getText().toString();
+        String vaccineBrand = vaccineBrandField.getText().toString();
+        String vaccineDate1 = vaccineDate1Field.getText().toString();
+        String vaccineDate2 = vaccineDate2Field.getText().toString();
+        String healthFacility = healthFacilityField.getText().toString();
+
+        HashMap patientProfile = new HashMap();
+        patientProfile.put("bio", bio);
+        patientProfile.put("age", age);
+        patientProfile.put("gender", gender);
+        patientProfile.put("contact", contact);
+        patientProfile.put("birthdate", birthDate);
+        patientProfile.put("address", address);
+        patientProfile.put("patientNumber", patientNumber);
+        patientProfile.put("experience", experience);
+        patientProfile.put("hospitalName", hospitalName);
+        patientProfile.put("hospitalAddress",hospitalAddress);
+        patientProfile.put("hospitalContactNumber", hospitalContactNumber);
+        patientProfile.put("vaccineCardNumber", vaccineCardNumber);
+        patientProfile.put("vaccineName", vaccineBrand);
+        patientProfile.put("vaccineDate1", vaccineDate1);
+        patientProfile.put("vaccineDate2", vaccineDate2);
+        patientProfile.put("healthFacility", healthFacility);
+
+        mDatabase = database.getReference("Medical Professional");
+        String targetReference = reference.replace(".", "");
+        parentMedicalNode = mDatabase.child(targetReference);
+        parentMedicalNode.updateChildren(patientProfile);
     }
 }
