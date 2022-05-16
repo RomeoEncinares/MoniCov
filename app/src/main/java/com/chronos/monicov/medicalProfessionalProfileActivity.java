5f2791f7ac1f2c64ea5af8c82567b43b54b6a2a1
Romeo Encinares <romeoncrs@gmail.com>
2022-05-16 16:23:26 +0800

    Fetch medical professional name, display first name, last name, and initial

--- app/src/main/java/com/chronos/monicov/medicalProfessionalProfileActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalProfileActivity.java
@@ -1,25 +1,53 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class medicalProfessionalProfileActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    private DatabaseReference mDatabase, parentPatientNode;
+    private FirebaseDatabase database;
+
+    Button updateButton;
     ImageButton homeButton, profileButton, medicalProfessionalButton, settingsButton, patientsButton;
+    TextView firstNameTextField, lastNameTextField, nameInitialTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_professional_profile);
 
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        firstNameTextField = findViewById(R.id.firstNameMedicalText);
+        lastNameTextField = findViewById(R.id.lastNameMedicalText);
+        nameInitialTextField = findViewById(R.id.nameInitialMedicalText);
+
         homeButton = findViewById(R.id.medicalHomeButton);
         profileButton = findViewById(R.id.medicalProfileButton);
         patientsButton = findViewById(R.id.medicalPatientsButton);
         medicalProfessionalButton = findViewById(R.id.medicalNotificationButton);
         settingsButton = findViewById(R.id.medicalSettingsButton);
+
+        String currentUser = getCurrentMedical();
+        queryData(currentUser);
 
         homeButton.setOnClickListener(view -> {
             startActivity(new Intent(medicalProfessionalProfileActivity.this, medicalProfessionalHomeActivity.class));
@@ -41,5 +69,33 @@ public class medicalProfessionalProfileActivity extends AppCompatActivity {
             startActivity(new Intent(medicalProfessionalProfileActivity.this, medicalProfessionalSettingsActivity.class));
         });
 
+    }
+
+    public String getCurrentMedical(){
+        String currentUser = mAuth.getCurrentUser().getEmail().toString();return currentUser;
+    }
+
+    public void queryData(String email) {
+        String emailKey = email.replace(".", "");
+        mDatabase = FirebaseDatabase.getInstance().getReference("Medical Professional").child(emailKey);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String, String> map = (HashMap<String, String>) snapshot.getValue();
+                String firstName = map.get("firstname");
+                String lastName = map.get("lastname");
+                char firstNameChar = firstName.charAt(0);
+                char lastNameChar = lastName.charAt(0);
+
+                firstNameTextField.setText(firstName);
+                lastNameTextField.setText(lastName);
+                nameInitialTextField.setText(String.valueOf(firstNameChar) + String.valueOf(lastNameChar));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
