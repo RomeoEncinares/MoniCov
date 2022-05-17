118515532a26504e1656d3b2bb0919ecc24cf5b0
Romeo Encinares <romeoncrs@gmail.com>
2022-05-17 14:25:24 +0800

    Fetch prescription data from firebase, feed into userPrescriptionAdapter to display in recycleview

--- app/src/main/java/com/chronos/monicov/medicalProfessionalPatientViewMedicationActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalPatientViewMedicationActivity.java
@@ -1,14 +1,31 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class medicalProfessionalPatientViewMedicationActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    UserPrescriptionsAdapter myAdapter;
+    ArrayList<User.Prescription> list;
+
     Button addMedicationButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +34,32 @@ public class medicalProfessionalPatientViewMedicationActivity extends AppCompatA
         addMedicationButton = findViewById(R.id.btnAddMedication);
 
         String chosenPatient = getIntent().getStringExtra("patient").toString();
+        System.out.println(chosenPatient);
+
+        recyclerView = findViewById(R.id.medicationList);
+        database = FirebaseDatabase.getInstance().getReference("Patient").child(chosenPatient).child("Medication");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new UserPrescriptionsAdapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User.Prescription userPrescription = dataSnapshot.getValue(User.Prescription.class);
+                    list.add(userPrescription);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         addMedicationButton.setOnClickListener(view -> {
             Intent passData = new Intent(getBaseContext(), medicalProfessionalPatientAddMedicationActivity.class);
