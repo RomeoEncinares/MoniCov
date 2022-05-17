a83b5fed5cd5f437b5bfcfe95ac0f9740c21139d
Romeo Encinares <romeoncrs@gmail.com>
2022-05-17 16:50:56 +0800

    Fetch prescription data from firebase, feed into userPrescription to display in recycleview

--- app/src/main/java/com/chronos/monicov/patientsPrescriptionsAcitivty.java
+++ app/src/main/java/com/chronos/monicov/patientsPrescriptionsAcitivty.java
@@ -1,14 +1,71 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class patientsPrescriptionsAcitivty extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    UserPrescriptionsAdapter myAdapter;
+    ArrayList<User.Prescription> list;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patients_prescriptions_acitivty);
+
+        mAuth = FirebaseAuth.getInstance();
+        String currentUser = getCurrentPatient();
+        currentUser = currentUser.replace(".", "");
+
+        recyclerView = findViewById(R.id.medicationList);
+        database = FirebaseDatabase.getInstance().getReference("Patient").child(currentUser).child("Medication");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new UserPrescriptionsAdapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User.Prescription userPrescription = dataSnapshot.getValue(User.Prescription.class);
+                    list.add(userPrescription);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
+
+    public String getCurrentPatient(){
+        String currentUser = mAuth.getCurrentUser().getEmail().toString();return currentUser;
+    }
+
 }
