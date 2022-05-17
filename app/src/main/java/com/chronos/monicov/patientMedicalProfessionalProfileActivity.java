e7e336aa1fe757c442cbc2a32118eecdd62d27b8
Romeo Encinares <romeoncrs@gmail.com>
2022-05-17 16:52:04 +0800

    Remove addPatient button

--- app/src/main/java/com/chronos/monicov/patientMedicalProfessionalProfileActivity.java
+++ app/src/main/java/com/chronos/monicov/patientMedicalProfessionalProfileActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class patientMedicalProfessionalProfileActivity extends AppCompatActivity {
 
     ImageButton homeButton, profileButton, statusButton, medicalProfessionalButton, settingsButton;
+    Button viewPrescriptionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_medical_professional_profile);
 
+        viewPrescriptionsButton = findViewById(R.id.btnViewPrescription);
+
         homeButton = findViewById(R.id.patientHomeButton);
         profileButton = findViewById(R.id.patientProfileButton);
         statusButton = findViewById(R.id.patientAddStatusButton);
         medicalProfessionalButton = findViewById(R.id.patientMedicalButton);
         settingsButton = findViewById(R.id.patientSettingsButton);
+
+        viewPrescriptionsButton.setOnClickListener(view -> {
+            startActivity(new Intent(patientMedicalProfessionalProfileActivity.this, patientsPrescriptionsAcitivty.class));
+        });
 
         homeButton.setOnClickListener(view -> {
             startActivity(new Intent(patientMedicalProfessionalProfileActivity.this, patientHomeActivity.class));
