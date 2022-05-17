9c762e77cc74c83daef51469580eac1566181f89
Romeo Encinares <romeoncrs@gmail.com>
2022-05-17 15:20:15 +0800

    Display patient name

--- app/src/main/java/com/chronos/monicov/medicalProfessionalPatientAddMedicationActivity.java
+++ app/src/main/java/com/chronos/monicov/medicalProfessionalPatientAddMedicationActivity.java
@@ -1,15 +1,20 @@
 package com.chronos.monicov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -20,6 +25,7 @@ public class medicalProfessionalPatientAddMedicationActivity extends AppCompatAc
     private FirebaseDatabase database;
 
     TextInputEditText medicineNameField, medicineDurationField, medicineTimeField;
+    TextView firstNameTextField, lastNameTextField;
     Button addMedicationButton;
 
     @Override
@@ -30,12 +36,17 @@ public class medicalProfessionalPatientAddMedicationActivity extends AppCompatAc
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 
+        firstNameTextField = findViewById(R.id.firstNameText);
+        lastNameTextField = findViewById(R.id.lastNameText);
+
         medicineNameField = findViewById(R.id.etMedicineName);
         medicineDurationField = findViewById(R.id.etMedicineDuration);
         medicineTimeField = findViewById(R.id.etMedicineTime);
         addMedicationButton = findViewById(R.id.btnAddMedication);
 
+
         String targetReference = getIntent().getStringExtra("patient").toString();
+        queryData(targetReference);
 
         addMedicationButton.setOnClickListener(view -> {
             checkFields(targetReference);
@@ -63,5 +74,27 @@ public class medicalProfessionalPatientAddMedicationActivity extends AppCompatAc
         statusListNode = currentPatientNode.child(medicineName);
         statusListNode.updateChildren(medicationMap);
         startActivity(new Intent(medicalProfessionalPatientAddMedicationActivity.this, medicalProfessionalPatientsActivity.class));
+    }
+
+    public void queryData(String email) {
+        String emailKey = email.replace(".", "");
+        mDatabase = FirebaseDatabase.getInstance().getReference("Patient").child(emailKey);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String, String> map = (HashMap<String, String>) snapshot.getValue();
+                String firstName = map.get("firstname");
+                String lastName = map.get("lastname");
+                char lastNameChar = lastName.charAt(0);
+
+                firstNameTextField.setText(firstName);
+                lastNameTextField.setText(String.valueOf(lastNameChar));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
